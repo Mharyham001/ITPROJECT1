350b7d16db7427b30ba819b89535f7acdea926b4
MARIAM <mariamtaiwo738@gmail.com>
2025-06-23 16:48:37 +0100

    june 23rd

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import product, order
+from django.contrib.auth.models import Group
+
+admin.site.site_header = 'AuxanoInventory Dashboard'
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'quantity')
+    list_filter = ('category',) 
+
 
 # Register your models here.
+admin.site.register(product, ProductAdmin)
+admin.site.register(order)
--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+CATEGORY = (
+    ('Stationary', 'Stationary'),
+    ('Electronics', 'Electronics'),
+    ('Accessories', 'Accessories'),
+)
+
+class product(models.Model):
+    name = models.CharField(max_length=100, null=True)
+    category = models.CharField(max_length=20, choices=CATEGORY)
+    quantity = models.IntegerField(null=True)
+
+    class Meta:
+        verbose_name = 'product'
+        verbose_name_plural = 'products'
+
+    def __str__(self):
+        return f'{self.name}-{self.quantity}'
+
+class order(models.Model):
+    product = models.ForeignKey(product, on_delete=models.CASCADE, null=True)
+    staff = models.ForeignKey(User, models.CASCADE, null=True)
+    order_quantity =models.PositiveIntegerField(null=True)
+    date = models.DateTimeField(auto_now_add=True, null=True)
+
+    class Meta:
+        verbose_name = 'order'
+        verbose_name_plural = 'orders'
+
+    def __str__(self):
+        return f'{self.product} ordered by {self.staff.username}'
+ 
