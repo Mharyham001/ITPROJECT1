b80d4fcad1fe9f4b4b64af84c7c4028d8d496be0
Mariam <mariamtaiwo738>
2025-06-24 16:05:06 +0100

    Updated Changes

--- myproject/urls.py
+++ myproject/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('dashboard.urls')),
     path('register/', user_views.register, name='user-register'),
-    path('login/', auth_views.Loginview.as_views(tem) , name='user-login'),
+    path('login/', auth_views.LoginView.as_view(template_name='user/login.html') 
+         , name='user-login'),
 ]
  
