a2834c2b9eb871447cf3b4ec64e85edf2c72ef03
Mariam <mariamtaiwo738>
2025-06-24 16:03:05 +0100

    Updated Changes

--- myproject/urls.py
+++ myproject/urls.py
@@ -17,11 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from user import views as user_views
-
-
+from django.contrib.auth import auth_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('dashboard.urls')),
     path('register/', user_views.register, name='user-register'),
+    path('login/', user_views.login_view, name='user-login'),
 ]
  
