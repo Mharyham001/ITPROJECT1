45b4d843b934648ba8bfb25fca7db3a2bcf37d07
MARIAM <mariamtaiwo738@gmail.com>
2025-06-24 13:47:17 +0100

    `Added crispy_forms and crispy_bootstrap4 to INSTALLED_APPS, updated settings.py and register.html templates, and modified user/views.py to use crispy forms and redirect to dashboard-index after registration.`

--- user/views.py
+++ user/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 
 
 def register(request):
-    return render(request, 'user/register.html')
+    if request.method == 'POST':
+       form =  UserCreationForm(request.post)
+       if form.is_valid():
+           form.save()
+    else:
+        form =  UserCreationForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'user/register.html', context)
