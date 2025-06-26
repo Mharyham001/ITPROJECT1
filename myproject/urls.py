0a2e112370a5b38394545fbaf51bef3873f0c65b
Mariam <mariamtaiwo738>
2025-06-26 20:36:18 +0100

    Updated Changes

--- myproject/urls.py
+++ myproject/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('dashboard.urls')),
     path('register/', user_views.register, name='user-register'),
+]
 
 =======
 <<<<<<< HEAD
