01b66469b83ee096a345f880b1bea5a786c3eb59
MARIAM <mariamtaiwo738@gmail.com>
2025-06-26 16:22:00 +0100

    JUNE26th 4:22

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,15 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from .models import product
 
 # Create your views here.
+
+@login_required(login_url='user-login')
 def index(request):
     return render(request, 'dashboard/index.html')
 
+@login_required(login_url='user-login')
 def staff(request):
      return render(request, 'dashboard/staff.html')
 
+@login_required(login_url='user-login')
 def product(request):
-     return render(request, 'dashboard/product.html')
+     items = product.object.all()
+     context ={
+          'items': items
+     }
+     return render(request, 'dashboard/product.html', context)
 
+@login_required(login_url='user-login')
 def order(request):
      return render(request, 'dashboard/order.html')
