820fb97e8be60a633b4096665c90b561bb7ba6c6
Mariam <mariamtaiwo738>
2025-06-26 20:37:18 +0100

    Updated Changes

--- myproject/urls.py
+++ myproject/urls.py
@@ -30,9 +30,8 @@ urlpatterns = [
 <<<<<<< HEAD
     path('profile/', user_views.profile, name='user-profile'),
     path('', auth_views.LoginView.as_view(template_name='user/login.html'), name='user-login'),
-=======
-<<<<<<< HEAD
->>>>>>> 3acd01a23e7a503a705bb908a506be5ebb99480b
+
+
     path('login/', auth_views.LoginView.as_view(template_name='user/login.html') 
          , name='user-login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='user/logout.html'), 
