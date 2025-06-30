2f3d4b6ea46dde6c9b94f93f9cff4e383866fd7f
Mariam <mariamtaiwo738>
2025-06-30 23:32:40 +0100

    Updated Changes

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
-from .models import ProductForm,
+from .models import ProductForm
+
+
 # Create your views here.
 
 @login_required(login_url='user-login')
