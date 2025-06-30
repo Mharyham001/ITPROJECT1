59ff751a5938644e4c3b673e681dd0dda5d574de
Mariam <mariamtaiwo738>
2025-06-30 23:12:40 +0100

    Updated Changes

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
-from .models import Product
 
 # Create your views here.
 
