03d84dc787c646f095fd247669d9b68d8e7ecb3f
Mariam <mariamtaiwo738>
2025-06-30 23:28:40 +0100

    Updated Changes

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -1,2 +1,9 @@
 from django import forms
-from .models import Product, Order
+from .models import Product, 
+
+
+class OrderForm(forms.ModelForm):
+
+    class Meta:
+        model = 
+        fields = ['name', 'order_quantity']
