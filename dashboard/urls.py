4eae6aba0691b56b695bdb63242a7f7339a44ef5
Mariam <mariamtaiwo738>
2025-07-01 00:29:41 +0100

    Updated Changes

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('dashboard/', views.index, name='dashboard-index'),
     path('staff/', views.staff, name='dashboard-staff'),
     path('product/', views.product, name='dashboard-product'),
-    path('product/delete', views.product, name='dashboard-product'),
+    path('product/delete/', views.product.delete, name='dashboard-product-delete'),
     path('order/', views.order, name='dashboard-order'),
 ]
