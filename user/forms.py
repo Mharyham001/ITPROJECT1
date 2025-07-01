2e31e7f27137eff65ab070c3fc32a3a85d406f24
MARIAM <mariamtaiwo738@gmail.com>
2025-07-01 13:49:46 +0100

    done

--- user/forms.py
+++ user/forms.py
@@ -3,10 +3,6 @@ from .models import Profile
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 9654802f352f96a2d64cd40f428950642c5acea8
 class CreateUserForm(UserCreationForm):
     email = forms.EmailField()
 
@@ -14,7 +10,6 @@ class CreateUserForm(UserCreationForm):
         model = User
         fields = ['username', 'email', 'password1', 'password2']
 
-<<<<<<< HEAD
 class UserUpdateForm(forms.ModelForm):
     class Meta:
         model = User
@@ -24,5 +19,3 @@ class ProfileUpdateForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ['phone', 'address', 'image']
-=======
->>>>>>> 9654802f352f96a2d64cd40f428950642c5acea8
