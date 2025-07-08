24e22e0c3230f4ce65b682f0c2b8e2f6f253c00a
MARIAM <mariamtaiwo738@gmail.com>
2025-07-08 12:29:56 +0100

    JULY 8

--- myproject/urls.py
+++ myproject/urls.py
@@ -24,7 +24,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('dashboard.urls')),
-<<<<<<< HEAD
     path('register/', user_view.register, name= 'user-register'),
     path('profile/', user_view.profile, name= 'user-profile'),
     path('profile/update', user_view.profile_update, name= 'user-profile-update'),
@@ -40,20 +39,3 @@ urlpatterns = [
   
          
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-=======
-    path('register/', user_views.register, name='user-register'),
-
-
-    path('profile/', user_views.profile, name='user-profile'),
-    path('profile/update/', user_views.profile_update, name='user-profile-update'),
-    path('', auth_views.LoginView.as_view(template_name='user/login.html'), name='user-login'),
-
-    path('logout/', auth_views.LogoutView.as_view(template_name='user/logout.html'), 
-         name='user-logout'),
-    path('login/', auth_views.LoginView.as_view(template_name='user/login.html'), name='user-login'),
-
-    path('logout/', auth_views.LogoutView.as_view(template_name='user/logout.html'), name='user-logout'),
-
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
- 
->>>>>>> da550b2cde998c51c11de720fd58833f4402d08d
